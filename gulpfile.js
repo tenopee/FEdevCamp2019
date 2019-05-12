c8b8980531f24d49016305ebc1dc054dd102866a
tenopee <tenotenotenopee@gmail.com>
2019-05-13 08:31:15 +0900

    [modify]設定を変更

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,28 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 
-
-gulp.task('uglify', function () {
-  gulp.src('./src/js/index.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist'));
-});
+// gulp.task('uglify', function () {
+//   gulp.src('./src/js/index.js')
+//     .pipe(uglify())
+//     .pipe(gulp.dest('./dist'));
+// });
 
 gulp.task('sass', function(){
-  gulp.src('./sass/index.scss')
+  gulp.src('/sass/index.scss')
     .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(gulp.dest('./src/css/'));
+    .pipe(gulp.dest('/src/css/'));
+});
+
+var gulp = require('gulp');
+
+// sass compiler
+var sass = require('gulp-sass');
+
+
+gulp.task('sass', function() {
+    gulp.src(path.join(assetsPath, '/sass/index.scss'))
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(path.join(assetsPath, '/src/css/')));
 });
